ab6ba9815e890d453efefa6a59ff0f7278570e80
u1_mas <mtakuya1022@gmail.com>
2025-01-29 00:48:02 +0900

    can login

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { logger } from "./logger.ts";
+import { launch } from "@astral/astral";
+
+const email = Deno.env.get("MONEYFORWARD_EMAIL");
+const password = Deno.env.get("MONEYFORWARD_PASSWORD");
+if (email === undefined || password === undefined) {
+  logger.error("not found email or password");
+  Deno.exit(1);
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const LOGIN_URL = "https://moneyforward.com/sign_in";
+const browser = await launch({
+  "headless": false,
+});
+const page = await browser.newPage();
+
+await page.goto(LOGIN_URL);
+const emailInput = await page.$('input[id="mfid_user[email]"]');
+await emailInput?.type(email);
+(await page.$('button[id="submitto"]'))?.click();
+
+await page.waitForNavigation();
+const passwordInput = await page.$('input[id="mfid_user[password]"]');
+await passwordInput?.type(password);
+(await page.$('button[id="submitto"]'))?.click();
+
+await page.waitForNavigation({
+  waitUntil: "networkidle2",
+});
+logger.info("login success");
